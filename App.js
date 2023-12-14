510800876a71bd5aef869c2bc579fee24ffd4c6b
Anelka <anelkadev1@gmail.com>
2023-12-14 18:53:44 +0100

    debug

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // Coding by Anelka MD 
 const touches = [...document.querySelectorAll('.boutton')];
-const listeKeycode = touches.map(touche => touche.dataset.key);
+const listeKeycode = touches.map(touches => touches.dataset.key);
 const ecran = document.querySelector('.ecran');
 
 document.addEventListener('keydown', (e) => {
